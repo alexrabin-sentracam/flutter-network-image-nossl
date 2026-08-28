5cd55651cd9647d20da8a00ceaf713361f5e6cb1
alexrabin-sentracam <160153899+alexrabin-sentracam@users.noreply.github.com>
2026-08-27 19:32:03 -0700

    Refactor LSNetworkImageNoSSL class methods and variables

--- networkimage_nossl.dart
+++ networkimage_nossl.dart
@@ -1,65 +1,76 @@
 import 'dart:async';
 import 'dart:io';
 import 'dart:ui' as ui;
+import 'dart:ui';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/painting.dart';
 
 class LSNetworkImageNoSSL extends ImageProvider<LSNetworkImageNoSSL> {
-  const LSNetworkImageNoSSL(this.url, {this.scale = 1.0, this.headers})
-      : assert(url != null),
-        assert(scale != null);
+  const LSNetworkImageNoSSL(this.url, {this.scale = 1.0, this.headers});
 
   final String url;
-
   final double scale;
-
   final Map<String, String>? headers;
 
   @override
   Future<LSNetworkImageNoSSL> obtainKey(ImageConfiguration configuration) {
-    return new SynchronousFuture<LSNetworkImageNoSSL>(this);
+    return SynchronousFuture<LSNetworkImageNoSSL>(this);
   }
 
   @override
-  ImageStreamCompleter load(LSNetworkImageNoSSL key, DecoderCallback decode) {
-    return MultiFrameImageStreamCompleter(codec: _loadasync(key), scale: key.scale);
+  ImageStreamCompleter loadImage(
+    LSNetworkImageNoSSL key,
+    ImageDecoderCallback decode,
+  ) {
+    return MultiFrameImageStreamCompleter(
+      codec: _loadAsync(key, decode),
+      scale: key.scale,
+    );
   }
 
-  static final HttpClient _httpclient = new HttpClient()
-    ..badCertificateCallback =
-        ((X509Certificate cert, String host, int port) => true);
+  static final HttpClient _httpClient = HttpClient()
+    ..badCertificateCallback = ((X509Certificate cert, String host, int port) =>
+        true);
 
-  Future<ui.Codec> _loadasync(LSNetworkImageNoSSL key) async {
+  Future<ui.Codec> _loadAsync(
+    LSNetworkImageNoSSL key,
+    ImageDecoderCallback decode,
+  ) async {
     assert(key == this);
 
     final Uri resolved = Uri.base.resolve(key.url);
-    final HttpClientRequest request = await _httpclient.getUrl(resolved);
+    final HttpClientRequest request = await _httpClient.getUrl(resolved);
     headers?.forEach((String name, String value) {
       request.headers.add(name, value);
     });
     final HttpClientResponse response = await request.close();
-    if (response.statusCode != HttpStatus.ok)
-      throw new Exception(
-          'http request failed, statuscode: ${response?.statusCode}, $resolved');
+    if (response.statusCode != HttpStatus.ok) {
+      throw Exception(
+        'HTTP request failed, statusCode: ${response.statusCode}, $resolved',
+      );
+    }
 
     final Uint8List bytes = await consolidateHttpClientResponseBytes(response);
-    if (bytes.lengthInBytes == 0)
-      throw new Exception('LSNetworkImageNoSSL is an empty file: $resolved');
+    if (bytes.lengthInBytes == 0) {
+      throw Exception('LSNetworkImageNoSSL is an empty file: $resolved');
+    }
 
-    return await ui.instantiateImageCodec(bytes);
+    final ImmutableBuffer buffer = await ImmutableBuffer.fromUint8List(bytes);
+    return decode(buffer);
   }
 
   @override
-  bool operator ==(dynamic other) {
+  bool operator ==(Object other) {
     if (other.runtimeType != runtimeType) return false;
-    final LSNetworkImageNoSSL typedother = other;
-    return url == typedother.url && scale == typedother.scale;
+    return other is LSNetworkImageNoSSL &&
+        other.url == url &&
+        other.scale == scale;
   }
 
   @override
-  int get hashcode => hashValues(url, scale);
+  int get hashCode => Object.hash(url, scale);
 
   @override
-  String tostring() => '$runtimeType("$url", scale: $scale)';
+  String toString() => '$runtimeType("$url", scale: $scale)';
 }
